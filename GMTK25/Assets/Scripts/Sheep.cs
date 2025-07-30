439948038781f86330f2442cfeeb9bfe17369617
inchdevelop <jillian.chapin@mymail.champlain.edu>
2025-07-30 16:44:54 -0400

    Version Change & Sheep Start

---
+++ GMTK25/Assets/Scripts/Sheep.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] CapsuleCollider2D capsuleCollider;
+    [SerializeField] SheepSO sheepSO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer.sprite = sheepSO.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+}
---
+++ GMTK25/Assets/Scripts/SheepSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SheepData", menuName = "ScriptableObjects/SheepData", order = 1)]
+public class SheepSO : ScriptableObject
+{
+    // Start is called before the first frame update
+
+    [SerializeField] public Sprite sprite;
+    [SerializeField] public int scoreValue;
+    [SerializeField] public float moveInterval;
+    [SerializeField] public SheepType type;
+    public enum SheepType
+    {
+        WHITE,
+        BLACK,
+        GOLD
+    }
+}
