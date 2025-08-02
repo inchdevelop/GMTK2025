d096f62ddd2ce77ca09a19c5a7a05b9cc53e494b
Jonathan <jonathan.ingles@mymail.champlain.edu>
2025-08-01 23:00:32 -0400

    dog movement

--- GMTK25/Assets/Scripts/playerInput.cs
+++ GMTK25/Assets/Scripts/playerInput.cs
@@ -6,7 +6,11 @@ public class playerInput : MonoBehaviour
 {
     Rigidbody2D rb;
     public float drag = 0.65f; //drag on dog so it slows
+    public float defaultDrag = 0.45f; //speed mult for dog
+    public float dragFall = 0.2f; //speed fall off for dog
+    public float dragMax = 20; //speed mult for dog
     public float defaultSpeed = 5; //default speed for dog
+    public float rotationSpeed = 250; //default speed for dog
     public float speed = 0; //speed for dog
     public float dashSpeed = 20; //speed mult for dog
     public float speedFall = 0.2f; //speed fall off for dog
@@ -32,6 +36,7 @@ public class playerInput : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         speed = defaultSpeed;
+        drag = defaultDrag;
     }
 
     // Update is called once per frame
@@ -44,16 +49,30 @@ public class playerInput : MonoBehaviour
         }
 
         //speed flat? code
-        if (speed >= defaultSpeed)
+        if (speed > defaultSpeed)
         {
             speed -= speedFall * Time.deltaTime;
-            if (speed >= defaultSpeed)
-                currentState = DogStates.DASH;
 
         }
         else
         {
             speed += speedFall * Time.deltaTime;
+        }
+        if (drag > defaultDrag)
+        {
+            drag -= dragFall * Time.deltaTime;
+
+        }
+        else
+        {
+            drag += dragFall * Time.deltaTime;
+        }
+        if (speed >= dashSpeed*0.9f)
+        {
+            currentState = DogStates.DASH;
+        }
+        else
+        {
             currentState = DogStates.RUN;
         }
 
@@ -67,19 +86,24 @@ public class playerInput : MonoBehaviour
         //input wasd and arrows
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-          rb.AddForce(Vector2.left * speed);
+            rb.AddForce(Vector2.left * speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector2.right), rotationSpeed * Time.deltaTime);
+            //transform.Rotate(Vector2.right);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(Vector2.right * speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector2.left), rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(Vector2.up * speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector2.down), rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(Vector2.down * speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector2.up), rotationSpeed * Time.deltaTime);
         }
 
         //dash code
@@ -93,6 +117,7 @@ public class playerInput : MonoBehaviour
             if (GameManager.instance.CheckDash())
             {
                 speed = dashSpeed;
+                drag = dragMax;
                 onPlayerDash?.Invoke();
             }
             yield return null;
